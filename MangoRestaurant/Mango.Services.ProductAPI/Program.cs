e05d699ea6c92cf7691c384bf47d231247a5ab9f
Raj <rajraval813@gmail.com>
2022-10-08 17:31:03 -0400

    Create and Impliment Reposetory and Dto

--- MangoRestaurant/Mango.Services.ProductAPI/Program.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DeafultConncetion")))
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DeafultConncetion")));
 
 // Old way 
 /*void ConfigurationServices(IServiceCollection services) 
---
+++ MangoRestaurant/Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> Getproducts();
+        Task<ProductDto> GetproductbyId(int ProductId);
+        Task<ProductDto> CreateUpdateproduct(ProductDto ProductDto);
+        Task<bool> Deleteproduct(int ProductId);
+
+    }
+}
