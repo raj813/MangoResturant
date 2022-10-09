df8f8a507ec6dc1e05042d34151319746f0535e2
Raj <rajraval813@gmail.com>
2022-10-09 00:16:19 -0400

    Add IProduct service ,Productdto and service dto (Reflecting one from Product API)

---
+++ MangoRestaurant/Mango.web/SD.cs
@@ -0,0 +1,16 @@
+﻿// Will hold static etails
+namespace Mango.web
+{
+    public class SD
+    {
+        public static string ProductAPIBase { get; set; }
+
+        public enum ApiType 
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
---
+++ MangoRestaurant/Mango.web/Services/IServices/IProductService.cs
@@ -0,0 +1,11 @@
+﻿namespace Mango.web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductAsync<T>();
+        Task<T> GetAllProductbyIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>();
+        Task<T> UpdateProductAsync<T>();
+        Task<T> DeleteProductAsync<T>(int id);
+    }
+}
